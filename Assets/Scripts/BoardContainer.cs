396981039e6349d33621fb60782fdf5c979bc6e2
Ionescu Eduard Robert <ionescueduardrobert@gmail.com>
2020-11-17 22:22:59 +0200

    some collider checks

--- Assets/Scripts/BoardContainer.cs
+++ Assets/Scripts/BoardContainer.cs
@@ -13,6 +13,7 @@ public class BoardContainer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0);
         currentCollider = this.gameObject.GetComponent<Collider2D>();
     }
 
@@ -21,13 +22,18 @@ public class BoardContainer : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
+            //if (Input.touchCount != 0) { Debug.Log(String.Format("touching : {0}", Input.touchCount)); }
             touch = Input.GetTouch(0);
+            
             touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
             if (touch.phase == TouchPhase.Ended)
             {
-                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
-                if (currentCollider == touchedCollider)
+                //Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                Debug.Log(String.Format("touching : {0}", touchPosition));
+                Debug.Log(String.Format("touching : {0}", currentCollider));
+                Debug.Log(String.Format("touching : {0}", currentCollider.bounds));
+                if (currentCollider.bounds.Contains(new Vector3(touchPosition.x, touchPosition.y, -1)))
                 {
                     Debug.Log(String.Format("Droped here :{0}.", this.gameObject.name));
                 }
